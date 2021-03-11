8811c4299a99fb3d43241670bd3b8ec589a35bbd
paolodelia99 <paolo.delia99@gmail.com>
2021-03-11 22:38:03 +0100

    Finnaly able to compile the contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,42 +1,112 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <=0.8.2;
 
-contract Lottery {
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+
+contract Lottery is Pausable {
 
     struct Participant {
         address addr;
     }
 
     struct LotteryPlay {
-        uint endBlock;
-        uint startBlock;
-        bytes32 blockHash;
+        uint256 totalAmount;
         address winner;
-        Participant[] partecipants;
+        Participant[] participants;
     }
 
     event LotteryFinished(address winner, uint256 amount);
     event Registered(address participantAddr);
 
-    uint128 maxParticipant;
-    uint currentLottery;
-    uint256 ticketAmount;
+    uint64 public maxParticipant;
+    uint128 public currentLottery;
+    uint256 public ticketAmount;
 
-    mapping(uint => LotteryPlay) lotteries;
+    mapping(uint128 => LotteryPlay) lotteries;
+    uint randNonce;
 
-    constructor(uint256 _ticketAmount) {
+    constructor(uint256 _ticketAmount, uint64 _maxParticipants) {
         if (_ticketAmount == 0) {
             ticketAmount = 0.02 ether;
         } else {
             ticketAmount = _ticketAmount;
         }
+
+        if (_maxParticipants == 0) {
+            maxParticipant = 20;
+        } else {
+            maxParticipant = _maxParticipants;
+        }
+
+        currentLottery = 0;
+
+        super._pause();
+
+        initLottery();
+
+        super._unpause();
     }
 
-    function setTicketAmount(uint256 _newTicketAmount) public {
+    function setTicketAmount(uint256 _newTicketAmount) external {
         if (_newTicketAmount != 0) {
             ticketAmount = _newTicketAmount;
         }
     }
 
+    function setMaxParticipants(uint64 _maxParticipants) external {
+        require(_maxParticipants != 0, "Partipants cannot be zero");
+
+        maxParticipant = _maxParticipants;
+    }
+
+    function register() external payable whenNotPaused returns (bool) {
+        require(lotteries[currentLottery].participants.length + 1 <= maxParticipant, "No more participants are allowed");
+        require(msg.value == ticketAmount, "The ticket value isn't the allowed one");
+
+        lotteries[currentLottery].participants.push(Participant(msg.sender));
+        emit Registered(msg.sender);
+        return true;
+    }
+
+    function initLottery() internal whenPaused {
+        require(address(this).balance == 0);
+        currentLottery++;
+    }
+
+    function getCurrentParticipants() external view returns (uint256) {
+        return lotteries[currentLottery].participants.length;
+    }
+
+    function runLottery() external {
+        require(lotteries[currentLottery].participants.length >= 2, "There must be at least partipants");
+
+        super._pause();
+
+        uint64 randomIndex = randMod(lotteries[currentLottery].participants.length);
+        address winner = lotteries[currentLottery].participants[randomIndex].addr;
+        address payable winnerPayable = address(uint160(winner));
+
+        uint256 winningAmount = address(this).balance;
+        if (winningAmount > 0) {
+            winnerPayable.transfer(winningAmount);
+        }
+
+        lotteries[currentLottery].winner = winner;
+        lotteries[currentLottery].totalAmount = winningAmount;
+
+        emit LotteryFinished(winner, winningAmount);
+
+        initLottery();
+
+        super._unpause();
+    }
+
+    function randMod(uint256 _modulus) internal returns(uint64) {
+        randNonce++;
+        return uint64(uint(keccak256(abi.encodePacked(block.timestamp,
+            msg.sender,
+            randNonce))) % _modulus);
+    }
 
 }
